67c2977344380a08739a7ae0520dfa06d29a23cc
Aylan Mello <aylanismello@gmail.com>
2017-10-24 00:31:53 -0700

    tests

--- server/index.js
+++ server/index.js
@@ -3,29 +3,34 @@
  */
 
 var express = require('express')
-  , routes = require('./routes')
-  , http = require('http');
+logger = require('morgan'),
+bodyParser = require('body-parser'),
+methodOverride = require('method-override'),
+  // , routes = require('./routes')
+http = require('http');
 
 var app = express();
 var server = app.listen(3000);
 var io = require('socket.io').listen(server); // this tells socket.io to use our express server
+const router = express.Router();
 
-app.configure(function(){
+// app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
+  app.use(logger('dev'));
   app.use(express.static(__dirname + '/public'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-});
+  app.use(bodyParser());
+  app.use(methodOverride());
+  app.use(router);
+// });
 
-app.configure('development', function(){
-  app.use(express.errorHandler());
-});
+// app.configure('development', function(){
+//   app.use(express.errorHandler());
+// });
 
-app.get('/', routes.index);
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
 
 
 console.log("Express server listening on port 3000");
