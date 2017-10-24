2f75275e71b98e1e48a67cf4c76354b7f477a49e
Aylan Mello <aylanismello@gmail.com>
2017-10-23 21:46:26 -0700

    worx

--- client/index.js
+++ client/index.js
@@ -2,75 +2,32 @@ var io = require('socket.io-client');
 var socket = io.connect('http://localhost:3000', {reconnect: true});
 
 // Add a connect listener
+const easymidi = require('easymidi');
+const inputs = easymidi.getInputs();
+const outputs = easymidi.getOutputs();
+
+  // 24 clock pulses per quarter note!
+  // 96 clock pulses per downbeat!
+const PULSES_PER_BEAT = 24;
+console.log(inputs);
+console.log(outputs);
+
+const traktor = new easymidi.Input(inputs[2]);
+let beat = 1;
+let tick = 0;
+let sound = 'tick';
+
 socket.on('connect', () => {
-    setInterval(() => {
-      // channel, from_address, data
-      socket.emit('CH01', 'me', 'test msg');
-    }, 1000)
+  traktor.on('clock', () => {
+    if(tick % PULSES_PER_BEAT === 0) {
+      socket.emit('CH01', 'me', beat);
+      console.log(beat);
+      if(beat === 4) {
+        beat = 1;
+      } else {
+        beat++;
+      }
+    }
+    tick++;
+  });
 });
-
-
-
-// const easymidi = require('easymidi');
-// const inputs = easymidi.getInputs();
-// const outputs = easymidi.getOutputs();
-// const io = require('socket.io-client');
-//
-// const PULSES_PER_BEAT = 24;
-// const socket = io.connect('http://localhost:3000', {reconnect: true});
-// console.log(inputs);
-// console.log(outputs);
-//
-// const traktor = new easymidi.Input(inputs[2]);
-// let beat = 1;
-// let tick = 0;
-// let sound = 'tick';
-
-//
-// traktor.on('clock', () => {
-//   if(tick % PULSES_PER_BEAT === 0) {
-//     console.log(beat);
-//     if(beat === 4) {
-//       beat = 1;
-//     } else {
-//       beat++;
-//     }
-//   }
-//   tick++;
-// });
-
-
-// socket.on('connect', function (socket) {
-//     console.log('Connected!');
-// });
-// socket.emit('CH01', 'me', 'test msg');
-
-// const ws = new WebSocket('ws://127.0.0.1:3000/socket');
-//
-// let MIDIStream = null;
-//
-// ws.on('open', () => {
-//   console.log('ssup!');
-//   ws.send('hello');
-//
-//   // 24 clock pulses per quarter note!
-//   // 96 clock pulses per downbeat!
-//
-//
-// });
-
-
-
-// client.on('open', () => {
-//   const s = client.createStream();
-//   s.write('sup');
-// });
-  // console.log('dope');
-  // MIDIStream = client.createStream();
-  // MIDIStream.on('data', (data) => {
-  //   console.log(`receive audio data ${data}`);
-  // });
-  //
-  // MIDIStream.on('end', (data) => {
-  //   console.log(`end ${data}`);
-  // });
--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 io.on('connection', (socket) => {
-   console.log('connection');
+   console.log('connected to server');
 
   socket.on('CH01', (from, msg) => {
     console.log('MSG', from, ' saying ', msg);
@@ -15,18 +15,3 @@ io.on('connection', (socket) => {
 http.listen(3000, () => {
   console.log('listening on *:3000');
 });
-
-// const app = require("express")();
-// const server = require("http").Server(app);
-// const io = require("socket.io")(server);
-//
-// const handleClient = function (socket) {
-//     // we've got a client connection
-//     socket.emit("tweet", {user: "nodesource", text: "Hello, world!"});
-// };
-//
-// io.on("connection", handleClient);
-//
-// server.listen(3000, () => {
-//   console.log('listening');
-// });
