70135501f89adc82c3f617e720888321e7e68990
Aylan Mello <aylanismello@gmail.com>
2017-10-27 19:08:01 -0700

    successfully record audio stream

--- client/audio_stream.js
+++ client/audio_stream.js
@@ -1,7 +1,19 @@
-var fs = require('fs');
-var audio = require('osx-audio');
+var record = require('node-record-lpcm16')
+var fs = require('fs')
 
-var input = new audio.Input();
+var file = fs.createWriteStream('test.wav', { encoding: 'binary' })
 
-var writable = fs.createWriteStream('output.wav');
-input.pipe(writable);
+record.start().pipe(file)
+
+// Stop recording after three seconds
+setTimeout(function () {
+  record.stop()
+}, 3000)
+
+// const fs = require('fs');
+// const audio = require('osx-audio');
+//
+// const input = new audio.Input();
+//
+// const writable = fs.createWriteStream('output.wav');
+// input.pipe(writable);
